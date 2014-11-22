717274b8be4f42c171bd736789c81d51fd5c2f5c
Gabrijel Mrgan <gabrijel.mrgan@infinum.hr>
2014-11-22 19:42:40 +0100

    added join tables

--- web/app/scripts/controllers/institution/home.js
+++ web/app/scripts/controllers/institution/home.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('rscwbApp')
-  .controller('InstitutionHomeCtrl', ['$scope',
-    function ($scope) {
+  .controller('InstitutionHomeCtrl', ['$scope','joinTables',
+    function ($scope, joinTables) {
+      $scope.blood = [];
+      //var currentInst = joinTables.join($scope.currentUser.id).then(function(instititution) {
+      var currentInst = joinTables.join("4AiydVM8JP").then(function(instititution) {
+        //console.log(instititution);
+        for(var i = 0; i < instititution.blood.length; i++) {
+          var att = instititution.blood[i].attributes;
+          $scope.blood[i] = { value: att.value, type: att.bloodType };
+        }
+      }, function(error) {
+        console.log(error);
+      })
 
     }]);
---
+++ web/app/scripts/services/jointables.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('rscwbApp')
+  .service('joinTables', ['$q', function ($q) {
+    var join = function(objectId) {
+      var User = Parse.Object.extend('User');
+      var Institution = Parse.Object.extend('InstitutionData');
+      var Blood = Parse.Object.extend('InstitutionBlood');
+
+      var queryUser = new Parse.Query(User);
+      var queryInstitution = new Parse.Query(Institution);
+      var queryBlood = new Parse.Query(Blood);
+
+      var fullInstitution = {};
+
+      var returnResult = $q.defer();+
+
+      queryUser.get(objectId, {
+        success: function(user) {
+          fullInstitution.user = user;
+
+          queryInstitution.equalTo('userObjectId', objectId);
+          queryInstitution.find({
+            success: function(institution) {
+              fullInstitution.institution = institution[0];
+
+              queryBlood.equalTo('userObjectId', objectId);
+              queryBlood.find({
+                success: function(blood) {
+                  fullInstitution.blood = blood;
+                  returnResult.resolve(fullInstitution);
+                },
+                error: function(result, error) {
+                  console.log(error);
+                  returnResult.reject(error);
+                }
+              })
+            },
+            error: function(result, error) {
+              console.log(error);
+              returnResult.reject(error);
+            }
+          })
+        },
+        error: function(result, error) {
+          console.log(error);
+          returnResult.reject(error);
+        }
+      });
+      return returnResult.promise;
+    };
+    return {
+      join: join
+    };
+
+  }]);
