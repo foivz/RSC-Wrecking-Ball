b551eb7379d2d105577026c208725597a5e45d29
Dino Kovač <dino.kovac@infinum.hr>
2014-11-22 20:14:52 +0100

    Implemented push notification support

--- android/LifeLine/app/src/main/java/hr/foi/rsc/lifeline/LifeLineApplication.java
+++ android/LifeLine/app/src/main/java/hr/foi/rsc/lifeline/LifeLineApplication.java
@@ -1,10 +1,14 @@
 package hr.foi.rsc.lifeline;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParsePush;
 import com.parse.ParseTwitterUtils;
+import com.parse.SaveCallback;
 
 /**
  * Created by dino on 22/11/14.
@@ -17,13 +21,24 @@ public class LifeLineApplication extends Application {
     public void onCreate() {
         instance = this;
 
+        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+
         Parse.initialize(this, "Qz1N1B4aBwzmiszChrGKU37QalVXzZ8iew6hV2oH",
             "kWAfX9HmPe1HRXojqj8kuPEufx0r9yXFcoCzVdAq");
         ParseTwitterUtils.initialize("GtQtnF894a6USOPRIVZx2HWfv",
             "yiG7Sn6ZTlVQbKSCtylH9LRmNCGA4Ir6jdNyKXdQMSGu9oQmwx");
         ParseFacebookUtils.initialize("830133423676900");
 
-        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+        ParsePush.subscribeInBackground("all", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 
     public LifeLineApplication getInstance() {
